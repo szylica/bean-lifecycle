80dc7efd392e33c04af7353c26eee4920a3581f0
szylica <szymonsta12@onet.pl>
2026-01-20 23:06:39 +0100

    zadanie1

---
+++ src/test/java/org/szylica/beanlifecycle/BeanLifecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.szylica.beanlifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanLifecycleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
